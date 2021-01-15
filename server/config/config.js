72a416b455bfc63c4d8d94f813a8c63d384ec434
tkrgolamdre <196w0007@itszongolica.edu.mx>
2021-01-14 21:53:45 -0600

    variable de entorno de mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://regina:DZM2xyzs7Zh4bmSC@cluster0.ria0k.mongodb.net/<dbname>?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 
