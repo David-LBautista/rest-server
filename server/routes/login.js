ecb7cc97ad1a38717ab0ae046d5b827dc9a6bc7f
tkrgolamdre <196w0007@itszongolica.edu.mx>
2021-01-15 19:15:03 -0600

    Peticion POST para loguearse

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,9 +9,43 @@ const Usuario = require('../models/usuario');
 
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true,
+
+    let body = req.body; //*Obtenemos el correo y password
+
+    //*verificamos si el correo es valido
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        //* vERIFICAMOS que los correos sean iguales
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "(Usuario) o contraseña incorrectos"
+                }
+            });
+        }
+        //* Verificamos que la contraseña introducida sea igual a la de la base de datos
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Usuario o (contraseña) incorrectos"
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token: '123abc'
+        });
     });
+
 });
 
 
